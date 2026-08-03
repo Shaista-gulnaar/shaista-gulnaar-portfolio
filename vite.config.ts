537a06365fd60a1b77049ea6a92ee7af29333f38
Shaista Gulnaar <shaistagulnaar.sg@gmail.com>
2026-08-03 17:33:53 -0500

    Set base path for portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shaista-gulnaar-portfolio/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
