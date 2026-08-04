d96d9805199d87818293aa2a31ae5a21a350b37c
Shaista Gulnaar <shaistagulnaar.sg@gmail.com>
2026-08-03 20:00:18 -0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shaista-gulnaar-portfolio/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
